86ca190afedd46121599f5c20f3e6bb14cd7778a
Sean C Davis <scdavis41@gmail.com>
2017-03-07 08:48:30 -0500

    DE2885 - Use Inline SVGs Show all available icons using the inline SVG, not a background image

--- src/app/ui-components/icons/inline-svg/inline-svg.component.ts
+++ src/app/ui-components/icons/inline-svg/inline-svg.component.ts
@@ -3,4 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: './inline-svg.component.html'
 })
-export class IconInlineComponent {}
+export class IconInlineComponent {
+
+  icons = [
+    'calendar',
+    'check-circle',
+    'chevron-left',
+    'chevron-right',
+    'circle-thin',
+    'contrast',
+    'facebook',
+    'github',
+    'twitter',
+    'instagram',
+    'usd',
+    'youtube'
+  ]
+
+}
