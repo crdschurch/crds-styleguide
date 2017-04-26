67c190a6b3555008f843bfc6ebbc7bbda132e7ab
Taylor MacDonald <tcmacdonald@gmail.com>
2017-04-26 16:33:42 -0400

    DE3347 XHR Race Conditions Kill redundant code

--- src/app/ui-components/tables/tables.component.spec.ts
+++ src/app/ui-components/tables/tables.component.spec.ts
@@ -4,8 +4,6 @@ import { BaseRequestOptions, Http  } from '@angular/http';
 import { ElementRef, Renderer } from '@angular/core';
 import { TablesComponent } from './tables.component';
 import { TablesJumbotronComponent } from './jumbotron/jumbotron.component';
-import { ContentBlockComponent } from '../../shared/content-block/content-block.component';
-import { ContentService } from '../../shared/content-block/content.service';
 
 describe('Component: Tables', () => {
   it('should create an instance', () => {
