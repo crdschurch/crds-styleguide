2c0bd3df99ca6bdf84d645e1d734c6f9480f4388
boggsey <eric@mlemedia.com>
2017-04-28 11:29:39 -0400

    US8001 - For Developers Section Adding the For Developers routes, content and navigation

--- src/app/developers/developers-routing.module.ts
+++ src/app/developers/developers-routing.module.ts
@@ -3,7 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DevelopersComponent } from './developers.component';
 import { DevelopersIntroComponent } from './intro/intro.component';
+import { BuiltOnBootstrapComponent } from './built-on-bootstrap/built-on-bootstrap.component';
+import { CodingStyleComponent } from './coding-style/coding-style.component';
+import { ContributionTipsComponent } from './contribution-tips/contribution-tips.component';
+import { CssSassComponent } from './css-sass/css-sass.component';
+import { CustomNpmPackagesComponent } from './custom-npm-packages/custom-npm-packages.component';
+import { FileStructureComponent } from './file-structure/file-structure.component';
+import { GettingStartedComponent } from './getting-started/getting-started.component';
+import { GridComponent } from './grid/grid.component';
+import { ImgixComponent } from './imgix/imgix.component';
+import { InstallationComponent } from './installation/installation.component';
+import { NamingConventionsComponent } from './naming-conventions/naming-conventions.component';
 import { Ng2ComponentsComponent } from './ng2-components/ng2-components.component';
+import { WebFontsComponent } from './web-fonts/web-fonts.component';
 
 const developerRoutes: Routes = [
   {
@@ -11,12 +23,60 @@ const developerRoutes: Routes = [
     component: DevelopersComponent,
     children: [
       {
-        path: '',
+        path: 'intro',
         component: DevelopersIntroComponent
+      },
+      {
+        path: 'built-on-bootstrap',
+        component: BuiltOnBootstrapComponent
+      },
+      {
+        path: 'coding-style',
+        component: CodingStyleComponent
+      },
+      {
+        path: 'contribution-tips',
+        component: ContributionTipsComponent
+      },
+      {
+        path: 'css-sass',
+        component: CssSassComponent
+      },
+      {
+        path: 'custom-npm-packages',
+        component: CustomNpmPackagesComponent
+      },
+      {
+        path: 'file-structure',
+        component: FileStructureComponent
+      },
+      {
+        path: 'getting-started',
+        component: GettingStartedComponent
+      },
+      {
+        path: 'grid',
+        component: GridComponent
+      },
+      {
+        path: 'imgix',
+        component: ImgixComponent
+      },
+      {
+        path: 'installation',
+        component: InstallationComponent
+      },
+      {
+        path: 'naming-conventions',
+        component: NamingConventionsComponent
       },
       {
         path: 'ng2-components',
         component: Ng2ComponentsComponent
+      },
+      {
+        path: 'web-fonts',
+        component: WebFontsComponent
       }
     ]
   }
---
+++ src/app/developers/file-structure/file-structure.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FileStructureComponent } from './file-structure.component';
+
+describe('Component: FileStructureComponent', () => {
+  it('should create an instance', () => {
+    let component = new FileStructureComponent();
+    expect(component).toBeTruthy();
+  });
+});
