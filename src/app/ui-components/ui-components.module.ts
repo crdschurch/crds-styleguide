a6d38f11f88040a47d63c576986cc07f0d4f577c
Sean C Davis <scdavis41@gmail.com>
2017-05-02 10:50:16 -0400

    US8063 - Component Search Fix routing issue

--- src/app/ui-components/ui-components.module.ts
+++ src/app/ui-components/ui-components.module.ts
@@ -78,11 +78,11 @@ import { TablesJumbotronComponent } from './tables/jumbotron/jumbotron.component
 /* forms */
 import { FormsComponent } from './forms/forms.component';
 import { FormsJumbotronComponent } from './forms/jumbotron/jumbotron.component';
-import { FormControlsComponent } from './forms/form-controls/form-controls.component';
-import { FormStatesComponent } from './forms/form-states/form-states.component';
+import { FormControlsComponent } from './forms/controls/form-controls.component';
+import { FormStatesComponent } from './forms/states/form-states.component';
 import { FormDatepickerComponent } from './forms/datepicker/datepicker.component';
 import { SearchFieldComponent } from './forms/search/search.component';
-import { FormGroupsComponent } from './forms/form-groups/groups.component';
+import { FormGroupsComponent } from './forms/groups/groups.component';
 
 /* sign-in */
 import { SignInComponent } from './sign-in/sign-in.component';
