6dd9a869e73a4e19b11fb3dbd7801946a25dc264
Taylor MacDonald <tcmacdonald@gmail.com>
2017-05-03 12:09:51 -0400

    US8001 Cleanup Developers Section Fix errors in specs

--- src/app/designers/designers.component.ts
+++ src/app/designers/designers.component.ts
@@ -6,16 +6,18 @@ import { Router } from '@angular/router';
   templateUrl: './designers.component.html'
 })
 export class DesignersComponent {
-  constructor(private router:Router) {}
+
+  constructor(private router:Router) {
+  }
 
   displaySidebar() {
-    return this.currentUrl() != '/developers/intro';
+    return this.currentUrl() !== '/developers/intro';
   }
 
   displaySidebarNav(id: string) {
     let url = this.currentUrl();
-    return (id == 'guidelines' && url.search(/intro|color-usage|copy-voice|logos|photography|typography/) > -1)
-        || (id == 'getting-started' && url.search(/mobile-first|prototyping|responsive-web-design/) > -1)
+    return (id === 'guidelines' && url.search(/intro|color-usage|copy-voice|logos|photography|typography/) > -1)
+        || (id === 'getting-started' && url.search(/mobile-first|prototyping|responsive-web-design/) > -1)
         ;
   }
 
--- src/app/developers/developers.component.ts
+++ src/app/developers/developers.component.ts
@@ -6,16 +6,17 @@ import { Router } from '@angular/router';
 })
 export class DevelopersComponent {
 
-  constructor(private router:Router) {}
+  constructor(private router:Router) {
+  }
 
   displaySidebar() {
-    return this.currentUrl() != '/developers/intro';
+    return this.currentUrl() !== '/developers/intro';
   }
 
   displaySidebarNav(id: string) {
     let url = this.currentUrl();
-    return (id == 'getting-started' && url.search(/getting-started|installation|coding-style|file-structure|naming-conventions|contribution-tips/) > -1)
-        || (id == 'learn-more' && url.search(/built-on-bootstrap|css-sass|grid|imgix|web-fonts|custom-npm-packages/) > -1)
+    return (id === 'getting-started' && url.search(/getting-started|installation|coding-style|file-structure|naming-conventions|contribution-tips/) > -1)
+        || (id === 'learn-more' && url.search(/built-on-bootstrap|css-sass|grid|imgix|web-fonts|custom-npm-packages/) > -1)
         ;
   }
 
