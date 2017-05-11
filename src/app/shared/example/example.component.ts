6d73a41721023f9ca486f29e1a2ef958f8493f9e
Taylor MacDonald <tcmacdonald@gmail.com>
2017-05-11 17:58:26 -0400

    US6869 Example Blocks Add syntax highlighting to example code block component

--- src/app/shared/example/example.component.ts
+++ src/app/shared/example/example.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Http, Response } from '@angular/http';
+
+let Prism = require('prismjs');
+var path = require('path');
 
 @Component({
   selector: 'crds-example',
@@ -8,6 +11,7 @@ import { Http, Response } from '@angular/http';
 })
 export class ExampleComponent implements OnInit {
 
+  private el: Element;
   private iframeSrc: SafeResourceUrl;
   private manifest: any;
   private rootPath: string = '/examples/'
@@ -21,13 +25,21 @@ export class ExampleComponent implements OnInit {
   @Input() width: string = '100%';
   @Input() height: string = '100';
 
-  constructor(private sanitizer: DomSanitizer, private http: Http) {
+  constructor(private sanitizer: DomSanitizer, private http: Http, private elementRef: ElementRef) {
+    this.el = <Element>this.elementRef.nativeElement;
   }
 
   ngOnInit() {
     this.path = `${this.rootPath}${this.id}/`
     this.markup = '<html></html>';
     this.http.get(`${this.path}manifest.json`).subscribe(this.parseManifest.bind(this));
+  }
+
+  addSyntaxHighlighting(language) {
+    let els = this.el.getElementsByTagName('pre')
+    for(let i = 0; i < els.length; i++) {
+      this.markup = Prism.highlight(this.markup, Prism.languages[language]);
+    }
   }
 
   parseManifest(res: Response) {
@@ -42,6 +54,18 @@ export class ExampleComponent implements OnInit {
   getMarkup(url) {
     this.http.get(url).subscribe((data: Response) => {
       this.markup = data['_body'];
+      let filetype;
+      switch(path.extname(data.url)) {
+        case '.css':
+          filetype = 'css';
+          break;
+        case '.js':
+          filetype = 'javascript'
+          break;
+        default:
+          filetype = 'markup';
+      }
+      this.addSyntaxHighlighting(filetype);
     });
   }
 
