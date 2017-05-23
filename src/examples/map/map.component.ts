ddb45e6bf904229bb859057159a80acd9be39545
Sean C Davis <scdavis41@gmail.com>
2017-05-23 13:42:03 -0400

    US7768 - Map Implementation Add our labels to the map

--- src/examples/map/map.component.ts
+++ src/examples/map/map.component.ts
@@ -21,25 +21,33 @@ export class MapComponent {
   disableDefaultUi = false;
   zoomControl = true;
 
+  // NOTE: Cluster icon available at: //crds-cms-uploads.s3.amazonaws.com/connect/CLUSTER.svg
+
   // markers: marker[] = [
   markers = <any> [
     {
+      name: 'person',
       lat: this.lat + 0.3,
       lng: this.lng + 0.3,
-      label: 'A',
-      draggable: false
+      iconUrl: '//crds-cms-uploads.s3.amazonaws.com/connect/PERSON.svg'
     },
     {
+      name: 'gathering',
       lat: this.lat - 0.3,
       lng: this.lng - 0.3,
-      label: 'B',
-      draggable: false
+      iconUrl: '//crds-cms-uploads.s3.amazonaws.com/connect/GATHERING.svg'
     },
     {
+      name: 'site',
       lat: this.lat - 0.45,
       lng: this.lng - 0.45,
-      label: 'C',
-      draggable: false
+      iconUrl: '//crds-cms-uploads.s3.amazonaws.com/connect/SITE.svg'
+    },
+    {
+      name: 'me',
+      lat: this.lat + 0.15,
+      lng: this.lng - 0.25,
+      iconUrl: '//crds-cms-uploads.s3.amazonaws.com/connect/ME.svg'
     }
   ];
 
