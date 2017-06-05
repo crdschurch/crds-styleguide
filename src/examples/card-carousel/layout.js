1a1a2a7b9ea525083de9b4b9bf83cc966f3c7710
zarahzachz <sarah.sachs@gmail.com>
2017-06-05 17:01:27 -0400

    US8392 - Card carousel Implement carousel feature on card decks - now with JS media query!

--- src/examples/card-carousel/layout.js
+++ src/examples/card-carousel/layout.js
@@ -1,13 +1,35 @@
-if (window.matchMedia('(min-width: 720px)').matches) {
+function carousel_styles() {
   const carousel = document.body.querySelector('.card-deck--carousel');
-
-  carousel.removeAttribute('data-flickity');
-  carousel.classList.remove('carousel');
-  carousel.classList.add('card-deck');
-
   const cards = carousel.querySelectorAll('.card');
 
-  for (var card = 0; card < cards.length; card++) {
-    cards[card].classList.remove('carousel-cell');
+  if (window.matchMedia('(max-width: 769px)').matches) {
+    carousel.classList.remove('card-deck');
+
+    new Flickity(carousel, {
+      cellAlign: 'left',
+      contain: true,
+      prevNextButtons: false,
+      pageDots: false
+    });
+
+    for (let card = 0; card < cards.length; card++) {
+      cards[card].classList.add('carousel-cell');
+    }
+  } else {
+    carousel.classList.add('card-deck');
+
+    new Flickity(carousel).destroy();
+
+    for (let card = 0; card < cards.length; card++) {
+      cards[card].classList.remove('carousel-cell');
+    }
   }
 }
+
+window.onload = function() {
+  carousel_styles();
+};
+
+window.onresize = function() {
+  carousel_styles();
+};
