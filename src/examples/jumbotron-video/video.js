6ff261218e571c276fa6baa0afb54c6c5f722b58
Sean C Davis <scdavis41@gmail.com>
2017-06-05 16:12:32 -0400

    US8483 - Jumbotron Video to DDK Add trigger that take the embedded player above the jumbotron content

--- src/examples/jumbotron-video/video.js
+++ src/examples/jumbotron-video/video.js
@@ -4,14 +4,22 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
-
-// playerId needs to match the ID of the player in the HTML file.
-var playerId = 'crds-bg-video';
-
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player(playerId, {
-    videoId: 'DzlH5SDGoyA', // the ID of the YouTube video
+
+  if (document.readyState != 'complete') {
+    setTimeout(onYouTubeIframeAPIReady, 100);
+    return true;
+  }
+
+  var videoId = 'DzlH5SDGoyA',
+      playerId = 'crds-bg-video',
+      playerEl = document.getElementById(playerId),
+      playerContainerEl = playerEl.parentElement,
+      jumbotronEl = playerContainerEl.parentElement,
+      videoTrigger = document.querySelector('.bg-video .video-trigger');
+
+  var player = new YT.Player(playerId, {
+    videoId: videoId, // the ID of the YouTube video
     playerVars: {
       autoplay: 1,
       controls: 0,
@@ -19,7 +27,7 @@ function onYouTubeIframeAPIReady() {
       loop: 1,
       playsinline: 1,
       showinfo: 0,
-      playlist: 'DzlH5SDGoyA' // See: https://stackoverflow.com/a/25781957/2241124
+      playlist: videoId // See: https://stackoverflow.com/a/25781957/2241124
     },
     events: {
       onReady: function(e) {
@@ -31,11 +39,8 @@ function onYouTubeIframeAPIReady() {
 
   // Control the size of the player so it fills the background.
   var resizeVideo = function() {
-    var playerEl = document.getElementById(playerId),
-        el = playerEl.parentElement,
-        containerEl = el.parentElement;
-        width = containerEl.offsetWidth,
-        height = containerEl.offsetHeight,
+    var width = jumbotronEl.offsetWidth,
+        height = jumbotronEl.offsetHeight,
         ratio = 16 / 9;
 
     // If the container is wider than the desired ratio ...
@@ -50,8 +55,8 @@ function onYouTubeIframeAPIReady() {
 
       // The player's container should sit at the left,
       // and at half of its excess height.
-      el.style.left = 0;
-      el.style.top = -((newHeight - height) / 2) + 'px';
+      playerContainerEl.style.left = 0;
+      playerContainerEl.style.top = -((newHeight - height) / 2) + 'px';
     }
     // If the player is higher than the aspect ratio
     else {
@@ -65,11 +70,24 @@ function onYouTubeIframeAPIReady() {
 
       // The player's container should sit at the top,
       // and at half of its excess width.
-      el.style.top = 0;
-      el.style.left = -((newWidth - width) / 2) + 'px';
+      playerContainerEl.style.top = 0;
+      playerContainerEl.style.left = -((newWidth - width) / 2) + 'px';
     }
   }
 
   // Watch for changes to the window's size and reset the video height.
   window.addEventListener('resize', resizeVideo, true);
+
+  // Overlay an embedded iframe player when clicking the trigger.
+  videoTrigger.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    var embedEl = document.createElement('iframe');
+    embedEl.setAttribute('class', 'video-full-size');
+    embedEl.setAttribute('src', 'https://www.youtube.com/embed/' + videoId + '?autoplay=1');
+    embedEl.setAttribute('frameborder', '0');
+
+    jumbotronEl.appendChild(embedEl);
+    return true;
+  }, true);
 }
