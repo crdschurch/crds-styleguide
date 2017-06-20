1cbbe7c1b319977b88e3610e78b608aac6ba01e1
Sean C Davis <scdavis41@gmail.com>
2017-06-20 15:53:07 -0400

    US8429 - Block Level Buttons Add mobile block level button documentation for utilities

---
+++ src/app/ui-components/utilities/buttons/buttons.component.spec.ts
@@ -0,0 +1,9 @@
+import { TestBed } from '@angular/core/testing';
+import { ButtonUtilitiesComponent } from './buttons.component';
+
+describe('Component: Buttons', () => {
+  it('should create an instance', () => {
+    let component = new ButtonUtilitiesComponent();
+    expect(component).toBeTruthy();
+  });
+});
