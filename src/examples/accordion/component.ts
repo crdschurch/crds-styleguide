7a3517e74817582617569e2dec89a4ae20537a99
zarahzachz <sarah.sachs@gmail.com>
2017-06-27 15:55:49 -0400

    US8553 - Filter drawers Clean up markup on accordion.

--- src/examples/accordion/component.ts
+++ src/examples/accordion/component.ts
@@ -3,11 +3,19 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <accordion>
-      <accordion-group heading="Static Header, initially expanded">
+    <accordion class="accordion">
+      <accordion-group>
+        <div accordion-heading>
+          Static Header, initially expanded
+          <i class="pull-right float-xs-right">
+            <svg class="icon icon-1" viewBox="0 0 256 256">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/svgs/icons.svg#chevron-down"></use>
+            </svg>
+          </i>
+        </div>
         This content is straight in the template.
       </accordion-group>
-      <accordion-group #group>
+      <accordion-group>
         <div accordion-heading>
           I can have markup, too!
           <i class="pull-right float-xs-right">
@@ -18,10 +26,26 @@ import {Component} from '@angular/core';
         </div>
         This is just some content to illustrate fancy headings.
       </accordion-group>
-      <accordion-group heading="Content 1">
+      <accordion-group>
+        <div accordion-heading>
+          Content 1
+          <i class="pull-right float-xs-right">
+            <svg class="icon icon-1" viewBox="0 0 256 256">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/svgs/icons.svg#chevron-down"></use>
+            </svg>
+          </i>
+        </div>
         <p>Content 1</p>
       </accordion-group>
-      <accordion-group heading="Content 2">
+      <accordion-group>
+        <div accordion-heading>
+          Content 2
+          <i class="pull-right float-xs-right">
+            <svg class="icon icon-1" viewBox="0 0 256 256">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/svgs/icons.svg#chevron-down"></use>
+            </svg>
+          </i>
+        </div>
         <p>Content 2</p>
       </accordion-group>
     </accordion>
