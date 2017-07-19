1be7b45415649bc0c7b4843f005f426b47a5736d
Sean C Davis <scdavis41@gmail.com>
2017-07-19 11:10:05 -0400

    US6546 - Timepicker to DDK Remove comments

--- src/app/ui-components/forms/timepicker/timepicker.component.ts
+++ src/app/ui-components/forms/timepicker/timepicker.component.ts
@@ -3,10 +3,4 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: './timepicker.component.html'
 })
-export class FormTimepickerComponent {
-  // public dt: Date = new Date();
-
-  // public today(): void {
-  //   this.dt = new Date();
-  // }
-}
+export class FormTimepickerComponent {}
