10648efb1329cc4ef8b5f0a069f0193ab3ca8f8d
Sean C Davis <scdavis41@gmail.com>
2017-07-26 14:36:24 -0400

    US9172 - Single Btn Dropdown Get basic dropdown working

---
+++ src/examples/button-dropdown/component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './component.html'
+})
+export class AppComponent {}
---
+++ src/examples/button-dropdown/module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { AppComponent } from './component';
+
+@NgModule({
+  imports: [BrowserModule, BsDropdownModule.forRoot()],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
