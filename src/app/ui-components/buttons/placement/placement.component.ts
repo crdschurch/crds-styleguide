3c353cf009bde4e7fa6d45f281740715689df0ce
Sean C Davis <scdavis41@gmail.com>
2017-08-17 15:28:53 -0400

    US9481 - Add Button Usage Page Put template inline

--- src/app/ui-components/buttons/placement/placement.component.ts
+++ src/app/ui-components/buttons/placement/placement.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './placement.component.html'
+  template: '<crds-content-block id="ddkButtonPlacementComponent"></crds-content-block>'
 })
 export class ButtonPlacementComponent {}
