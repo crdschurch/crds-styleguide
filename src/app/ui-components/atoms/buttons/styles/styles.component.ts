54c96f340800bac6a40847465229fde6eadf6692
Taylor MacDonald <tcmacdonald@gmail.com>
2017-10-20 16:58:32 -0400

    US9479 Linkable Headers Remove unnecessary update

--- src/app/ui-components/atoms/buttons/styles/styles.component.ts
+++ src/app/ui-components/atoms/buttons/styles/styles.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-import { LinkableHeaderService } from '../../../../services/linkable-header.service';
 
 @Component({
   templateUrl: './styles.component.html'
 })
-export class ButtonStylesComponent {
-  constructor(private headers: LinkableHeaderService) {
-  }
-}
+export class ButtonStylesComponent {}
