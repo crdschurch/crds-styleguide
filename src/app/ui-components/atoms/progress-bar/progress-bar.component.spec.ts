6dee4e388f373b083361107c96d3c27a5aed239f
Sean C Davis <scdavis41@gmail.com>
2017-12-15 11:33:12 -0500

    US11066 - Progress Bar to DDK Fix specs

--- src/app/ui-components/atoms/progress-bar/progress-bar.component.spec.ts
+++ src/app/ui-components/atoms/progress-bar/progress-bar.component.spec.ts
@@ -1,25 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, async } from '@angular/core/testing';
 import { ProgressBarComponent } from './progress-bar.component';
 
-describe('ProgressBarComponent', () => {
-  let component: ProgressBarComponent;
-  let fixture: ComponentFixture<ProgressBarComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProgressBarComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProgressBarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+describe('Component: Dashboard', () => {
+  it('should create an instance', () => {
+    let component = new ProgressBarComponent();
     expect(component).toBeTruthy();
   });
 });
