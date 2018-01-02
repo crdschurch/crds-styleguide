8f249704cf25188f908d6875022a37f2046898f5
Sean C Davis <scdavis41@gmail.com>
2018-01-02 14:56:16 -0500

    US11065 - Charts to DDK Add basic chart example

--- src/examples/charts/chart.component.ts
+++ src/examples/charts/chart.component.ts
@@ -6,6 +6,31 @@ import { Component } from '@angular/core';
 })
 export class ChartComponent {
 
-  constructor() {}
+  bars = {
+    financial: 0.6,
+    intellectual: 1.2,
+    physical: 1.8,
+    relational: 2.4,
+    spiritual: 2.7
+  };
+
+  public barPath(name, top, sectionWidth) {
+    const val = this.bars[name];
+    const right = ((val * sectionWidth) + 82) - 4;
+    return [
+      `M82,${top}`,
+      `L${right},${top}`,
+      `C${right + 2},${top}`,
+      `${right + 4},${top + 2}`,
+      `${right + 4},${top + 4}`,
+      `L${right + 4},${top + 45}`,
+      `C${right + 4},${top + 47}`,
+      `${right + 2},${top + 49}`,
+      `${right},${top + 49}`,
+      `L82,${top + 49}`,
+      `L82,0`,
+      `Z`
+    ].join(' ');
+  }
 
 }
