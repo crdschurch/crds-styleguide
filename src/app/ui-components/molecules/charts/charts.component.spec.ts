09375ae0857b2c8625441dd098ddacb5712dca19
Sean C Davis <scdavis41@gmail.com>
2018-01-02 17:12:35 -0500

    US11065 - Charts to DDK Fix specs

---
+++ src/app/ui-components/molecules/charts/charts.component.spec.ts
@@ -0,0 +1,9 @@
+import { TestBed, async } from '@angular/core/testing';
+import { ChartsComponent } from './charts.component';
+
+describe('Component: ChartsComponent', () => {
+  it('should create an instance', () => {
+    let component = new ChartsComponent();
+    expect(component).toBeTruthy();
+  });
+});
