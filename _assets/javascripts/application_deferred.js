829a1e91679cbfbaae0e68f7bda6be0f8eb90ccc
zarahzachz <sarah.sachs@gmail.com>
2018-09-16 10:15:17 -0400

    Rename tooltip JS file to have more context.

--- _assets/javascripts/application_deferred.js
+++ _assets/javascripts/application_deferred.js
@@ -1,5 +1,5 @@
 //= require ./vendor/imgix.min
 //= require ./vendor/jquery-3.3.1.min
 //= require ./vendor/bootstrap.min
-//= require ./components/tooltips
+//= require ./components/tooltips-example
 //= require ./components/dark-theme
