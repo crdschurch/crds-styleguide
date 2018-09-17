4a8a6f8641dfab68bda773e3f496b8f04fc327b7
zarahzachz <sarah.sachs@gmail.com>
2018-09-17 10:15:57 -0400

    Add title frontmatter to searchable pages and update search JSON file.

---
+++ _assets/javascripts/components/site-search.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // Navigating search:
+    // If user types '/', focus search
+    // If user types '↓', then move focus down search list
+    // If user types '↑', then move focus up search list
+    // If user types 'enter', then url directs to selected list option
+
+    // Search:
+    // Key matching against json list of components (search-index.json)
+});
