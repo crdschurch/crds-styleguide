dc1c017ffa73bb251ee74eaa9f82fa99f55f8ead
zarahzachz <sarah.sachs@gmail.com>
2018-09-18 12:08:35 -0400

    DE5876 - Search index Refactor search results JSON to data dir per Jekyll docs.

--- _plugins/filters/search.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module SearchFilter
-    # def asset_url(input)
-    #   "http://www.example.com/#{input}?#{Time.now.to_i}"
-    # end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::SearchFilter)
